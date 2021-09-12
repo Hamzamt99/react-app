223decb5d3b55483d1ea1c59c537aa9ecf4444d1
Hamzamt99 <tamarihamza4@gmail.com>
2021-09-12 22:12:43 +0300

    Attemp1

---
+++ src/components/Horned.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Main from './Main';
+
+
+class Horned extends React.Component {
+  render() {
+    return (
+      <>
+        
+                
+     <Main HornedTitle={'UniWhal'}/>
+     <Main Hornedimg={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"'}/>
+     <Main Horneddes={'A unicorn and a narwhal nuzzling their horns'}/>
+
+       
+        
+      </>
+    )
+  }
+}
+
+export default Horned;
